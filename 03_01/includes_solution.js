abd5ded02585639c32055f2fba679cb1a5cda761
brunaportelaa <brunaporteladj@gmail.com>
2024-05-18 12:55:43 -0300

    Solved challenges for chapter 3 and 4 and deleted unnecessary files from these chapters

--- 03_01/includes_solution.js
+++
@@ -1,5 +0,0 @@
-const careers = ["teacher", "doctor", "engineer", "nurse", "scientist"];
-
-//Does the careers array include a nurse?
-const nurseIncluded = careers.includes("nurse");
-console.log("Is a nurse included in the array", nurseIncluded)
--- 03_03_Challenge/evaluating_data_for_a_single_value_challenge.js
+++ 03_03_Challenge/evaluating_data_for_a_single_value_challenge.js
@@ -3,9 +3,13 @@ const bowlingScores = [154, 204, 300, 184, 286];
 
 //Challenge Question #1
 //Does the array of bowling scores include a 300?
+console.log(bowlingScores.includes(300))
 
 //Challenge Question #2
 //Are some of the scores under 150?
+console.log(bowlingScores.some(e => e<150))
 
 //Challenge Question #3
 //Is every score an even number?
+console.log(bowlingScores.every(e => { return e % 2 === 0 } ))
+
